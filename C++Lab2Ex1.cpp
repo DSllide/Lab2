8a0dad67885ddc051e6669d444b8d0417d8306c1
DSllide <dreatsllide@gmail.com>
2025-05-29 17:18:58 +0300

    Add files via upload

---
+++ C++Lab2Ex1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+
+    cout << "Character: " << ch << endl;
+
+
+    cout << "ASCII code (decimal): " << static_cast<int>(ch) << endl;
+
+
+    cout << "ASCII code (hex): 0x" << hex << uppercase << static_cast<int>(ch) << endl;
+
+    return 0;
+}
