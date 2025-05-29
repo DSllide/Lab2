47d699f0069fab834a51055e07465fd3a5078c27
DSllide <dreatsllide@gmail.com>
2025-05-29 17:25:05 +0300

    Add files via upload

---
+++ C++Lab2Ex3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter an integer number: ";
+    cin >> number;
+
+    cout << "Even: " << (number % 2 == 0 ? "YES" : "NO") << endl;
+    cout << "Divisible by 8: " << (number % 8 == 0 ? "YES" : "NO") << endl;
+    cout << "Divisible by 16: " << (number % 16 == 0 ? "YES" : "NO") << endl;
+
+    cout << "Octal format: " << oct << number << endl;
+    cout << "Hexadecimal format: " << hex << number << endl;
+
+    return 0;
+}
